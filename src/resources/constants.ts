3cbdce56d048fc56e72549b977aa7b769d5bf0c3
PCB <perbjester@gmail.com>
2022-09-11 21:37:13 +0200

    thumb.bug

--- src/resources/constants.ts
+++ src/resources/constants.ts
@@ -1,3 +1,3 @@
 export const IMAGE_BASE_URL = process.env.REACT_APP_PR_API_HOST + 'img';
-export const personThumbnailFolder = '/api/images/persons/thumbs/';
+export const personThumbnailFolder = '/api/images/persons/thumbs/thumbnail.';
 export const personImageFolder = '/api/images/persons/medium/';
