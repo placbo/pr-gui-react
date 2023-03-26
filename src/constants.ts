5c4f99d23af6fda831104493a9d8182950e675f1
PCB <perbjester@gmail.com>
2023-03-26 19:58:47 +0200

    baseurl

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,15 @@
 export const USE_MOCK_DATA = process.env.REACT_APP_USE_MOCK === 'true';
 export const BCRYPT_SALT = process.env.REACT_APP_BCRYPT_SALT;
 
-export const BASE_URL = process.env.REACT_APP_PR_API_HOST;
-export const LOGIN_URL = BASE_URL + '/api/auth/login';
-export const PERSONS_URL = BASE_URL + '/api/persons';
-export const IMAGES_URL = BASE_URL + '/api/images';
-export const IMAGE_URL = BASE_URL + '/api/image';
-export const COMMUNITIES_URL = BASE_URL + '/api/communities';
-export const IMAGE_UPLOAD_URL = BASE_URL + '/api/imageupload';
+export const API_BASE_URL = process.env.REACT_APP_PR_API_HOST ?? '';
 
+export const LOGIN_URL = API_BASE_URL + '/auth/login';
+export const PERSONS_URL = API_BASE_URL + '/persons';
+export const IMAGE_URL = API_BASE_URL + '/image';
+export const COMMUNITIES_URL = API_BASE_URL + '/communities';
+export const IMAGE_UPLOAD_URL = API_BASE_URL + '/imageupload';
+
+export const IMAGES_URL = API_BASE_URL + '/images';
 export const PERSON_THUMBNAIL_URL = IMAGES_URL + '/persons/thumbs/thumbnail.';
 export const PERSON_IMAGES_MEDIUM_URL = IMAGES_URL + '/persons/medium/';
 export const PERSON_IMAGE_URL = IMAGES_URL + '/persons/';
